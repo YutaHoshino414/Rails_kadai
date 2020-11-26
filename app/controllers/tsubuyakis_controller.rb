3a9c363048b52ab74b73fcc27f41e2c914a91072
Yuta Hoshino <y.hoshino25@gmail.com>
2020-11-26 19:16:18 +0900

    新規投稿画面・機能の作成

--- app/controllers/tsubuyakis_controller.rb
+++ app/controllers/tsubuyakis_controller.rb
@@ -1,4 +1,13 @@
 class TsubuyakisController < ApplicationController
   def index
   end
+
+  def new
+    @tsubuyaki = Tsubuyaki.new
+  end
+
+  def create
+    Tsubuyaki.create(content: params[:tsubuyaki][:content])
+    redirect_to new_tsubuyaki_path
+  end
 end
