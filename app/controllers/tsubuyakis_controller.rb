74dadd77afea4a431aa0e640b79ac562bc2e8978
Yuta Hoshino <y.hoshino25@gmail.com>
2020-11-26 19:31:57 +0900

    ツイート一覧表示機能の追加

--- app/controllers/tsubuyakis_controller.rb
+++ app/controllers/tsubuyakis_controller.rb
@@ -1,5 +1,6 @@
 class TsubuyakisController < ApplicationController
   def index
+    @tsubuyakis = Tsubuyaki.all
   end
 
   def new
@@ -7,7 +8,12 @@ class TsubuyakisController < ApplicationController
   end
 
   def create
-    Tsubuyaki.create(content: params[:tsubuyaki][:content])
+    Tsubuyaki.create(tsubuyaki_params)
     redirect_to new_tsubuyaki_path
+  end
+
+  private
+  def tsubuyaki_params
+    params.require(:tsubuyaki).permit(:content)
   end
 end
